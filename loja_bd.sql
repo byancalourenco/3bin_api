01c8bd2786852ae5e04fb0e090ae3ad2d86f83e9
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2026-08-27 09:24:51 -0300

    Add files via upload

--- loja_bd.sql
+++ loja_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Ago-2026 às 15:33
+-- Tempo de geração: 27-Ago-2026 às 14:24
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -26,6 +26,30 @@ USE `loja`;
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `filmes`
+--
+
+CREATE TABLE `filmes` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(100) NOT NULL,
+  `diretor` varchar(100) NOT NULL,
+  `genero` varchar(100) NOT NULL,
+  `duracao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `filmes`
+--
+
+INSERT INTO `filmes` (`id`, `titulo`, `diretor`, `genero`, `duracao`) VALUES
+(1, 'Se Eu Fosse Você', 'Daniel Filho', 'Comédia romântica', 108),
+(2, 'Guardiões da Galáxia', 'James Gunn', 'Ação/Ficção científica', 121),
+(3, 'Homem-Aranha', 'Sam Raimi', 'Ação/Aventura', 121),
+(4, 'Vingadores: Ultimato', 'Anthony Russo, Joe Russo', 'Ação/Ficção científica', 181);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `produtos`
 --
 
@@ -41,11 +65,18 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `preco`, `quantidade`) VALUES
-(1, 'Computador', 2500, 10);
+(1, 'Computador', 1000, 20);
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `filmes`
+--
+ALTER TABLE `filmes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `ix_filmes_id` (`id`);
 
 --
 -- Índices para tabela `produtos`
@@ -57,6 +88,12 @@ ALTER TABLE `produtos`
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `filmes`
+--
+ALTER TABLE `filmes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
